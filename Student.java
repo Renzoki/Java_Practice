70178a721571428f47ec6f88d39dafd4f89c8aa4
Renzoki <renzonifico@gmail.com>
2023-12-22 15:57:37 +0800

    uno

---
+++ Student.java
@@ -0,0 +1,31 @@
+//Encapsulation Practice
+public class Student {
+    private int student_ID;
+    private String student_name;
+    private double student_Grades;
+
+    public void setID(int ID){
+        this.student_ID = ID;
+    }
+
+    public void setName(String name){
+        this.student_name = name;
+    }
+
+    public int getID(){
+        return student_ID;
+    }
+
+    public String getName(){
+        return student_name;
+    }
+
+    public void addGrade(double grade){
+        if(grade >= 0 && grade <= 100){
+            this.student_Grades = grade;
+        }
+        else{
+            System.out.println("\nInvalid grade\n");
+        }
+    }
+}
