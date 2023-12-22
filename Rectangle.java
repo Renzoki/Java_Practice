3fcd64b41473589d00985d8f52cfeec861519e31
Renzoki <renzonifico@gmail.com>
2023-12-22 15:34:23 +0800

    Uno

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+//Encapsulation Practice
+public class Rectangle {
+    private int length, width;
+
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+}
