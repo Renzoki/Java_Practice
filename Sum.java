29f79d5aae152fb749e727f22c1176f1e73c7aa4
Renzoki <renzonifico@gmail.com>
2023-12-30 23:07:11 +0800

    Tres

--- Sum.java
+++ Sum.java
@@ -1,10 +1,10 @@
 public class Sum {
     private int[][] A; //2D array "A"
-    private int i, j;
     private int Sum = 0;
 
     public Sum(int[][] A, int length, int width){
         this.A = new int[length][width];
+        int i, j;
         for(i = 0; i < length; i++){
             for(j = 0; j < width; j++){
                 Sum += A[i][j];
